7f66714cb63c0534336dcc46ed82f485d3b0890b
iskakanartur <artdar14@gmail.com>
2024-04-08 13:43:24 +0400

    Added import from hiistoric fasted data csv jupyter notebook

--- app/models.py
+++ app/models.py
@@ -46,7 +46,7 @@ class body(db.Model):
 
 class eat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    meal = db.Column(db.String(500), nullable=False)
+    meal = db.Column(db.String(500), nullable=True)
     comment = db.Column(db.String(150), nullable=True)
     time_delta = db.Column(db.Interval)
     date_added = db.Column(db.DateTime(timezone=True), server_default=func.now())
